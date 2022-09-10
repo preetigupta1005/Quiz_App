c296b4fb792d59e8d31f5f9aefe3720ce12d1255
preetigupta1005 <113229886+preetigupta1005@users.noreply.github.com>
2022-09-10 14:20:28 +0530

    Files Upload

---
+++ app/src/main/java/com/example/helloworld/QuestionData.kt
@@ -0,0 +1,19 @@
+package com.example.helloworld
+
+data class QuestionData (
+    var id:Int,
+    var question:String,
+    var optionone:String,
+    var optiontwo:String,
+    var optionthree:String,
+    var optionfour:String,
+    var correctans:Int,
+
+
+
+
+    )
+
+
+
+
